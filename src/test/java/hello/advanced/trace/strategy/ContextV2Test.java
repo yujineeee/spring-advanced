3665abbf16aa09f5461440217dfa3d450ba40d6e
장유진/전시개발팀 <yujin.jang@musinsa.com>
2021-11-26 13:11:57 +0900

    ch.3-2 전략 패턴

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,53 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.ContextV2;
+import hello.advanced.trace.strategy.code.Strategy;
+import hello.advanced.trace.strategy.code.StrategyLogic1;
+import hello.advanced.trace.strategy.code.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /*
+     * 전략패턴사용*/
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    /*
+     * 전략패턴사용 - 익명이 */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    /*
+     * 전략패턴사용 - 람다.. 크.. */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+
+
+}
---
+++ src/test/java/hello/advanced/trace/strategy/code/Strategy.java
@@ -0,0 +1,5 @@
+package hello.advanced.trace.strategy.code;
+
+public interface Strategy {
+    void call();
+}
